69d7752865acdc854d2e700b3ec55f9c791ed931
boski <boskitechh@gmail.com>
2022-08-31 15:26:50 +0100

    push commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import {BrowserRouter as Switch, Routes, Route} from 'react-router-dom'
 import Card from './pages/Card';
-import Home from './pages/Home';
 
 function App() {
   return (
     <Switch >
       <Routes>
-        <Route path='/' exact element={<Home/>} />
-        <Route path='/card' exact element={<Card/>} />
+        <Route path='/' exact element={<Card/>} />
       </Routes>
     </Switch>
   );
